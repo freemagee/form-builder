d60cc0272fc34bb2c0de63ebd78192e190e9f576
freemagee <1529405+freemagee@users.noreply.github.com>
2019-08-16 12:49:48 +0100

    Updates

---
+++ precache-manifest.269b19177a706a357d008bc708a6f0bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6d3888cb670309bb55cb39090b9c4e",
+    "url": "/form-builder/index.html"
+  },
+  {
+    "revision": "29d271b04bd9782b4382",
+    "url": "/form-builder/static/css/main.40ab9ff8.chunk.css"
+  },
+  {
+    "revision": "d1440722f12fb6fc5fef",
+    "url": "/form-builder/static/js/2.f932e4a8.chunk.js"
+  },
+  {
+    "revision": "29d271b04bd9782b4382",
+    "url": "/form-builder/static/js/main.42c121f5.chunk.js"
+  },
+  {
+    "revision": "85d09670639038fbe42e",
+    "url": "/form-builder/static/js/runtime~main.a1c1bca4.js"
+  }
+]);
